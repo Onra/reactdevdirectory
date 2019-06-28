efea8af621fb4ab185f63e350d3285e6e21cc40d
Arnaud Christ <arnaud.christ@gmail.com>
2019-06-28 06:10:20 +0200

    Create code structure.

---
+++ src/layouts/SectionWelcome/SectionWelcome.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SectionWelcomeStyled from "./styles/SectionWelcomeStyled";
+
+const SectionWelcome = () => (
+  <SectionWelcomeStyled>React Dev Repository</SectionWelcomeStyled>
+);
+
+export default SectionWelcome;
---
+++ src/layouts/SectionWelcome/styles/SectionWelcomeStyled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const SectionWelcomeStyled = styled.section`
+  background: linear-gradient(45deg, #3498db, #2980b9);
+  color: white;
+`;
+
+export default SectionWelcomeStyled;
