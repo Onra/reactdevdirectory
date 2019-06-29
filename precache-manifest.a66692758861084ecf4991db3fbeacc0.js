f6cecad96528699f962deb38bb8779f63822e727
Arnaud Christ <arnaud.christ@gmail.com>
2019-06-29 18:37:28 +0200

    Updates

---
+++ precache-manifest.a66692758861084ecf4991db3fbeacc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b748f95bf38df4c33d14848e7ea6a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3b31774ca441be9280c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b549fd30bc4beac514b2",
+    "url": "/static/js/2.7fad34e3.chunk.js"
+  },
+  {
+    "revision": "d3b31774ca441be9280c",
+    "url": "/static/js/main.02c110fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
